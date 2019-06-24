91c62a036936ae1056b48d80044f3c9bd6648f69
Fernando Zendron <fernando.zendron@becommerce.com.br>
2019-06-23 22:41:55 -0300

    First commit

---
+++ src/main/java/com/zendron/callcontrol/CallcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.zendron.callcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallcontrolApplication.class, args);
+	}
+
+}
